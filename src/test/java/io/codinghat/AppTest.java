21eeaff7cba31f8096267bd910dc843174d74df2
Himanshu Pandey <himanshu.tech10@gmail.com>
2018-01-18 13:16:45 +1100

    tests added

--- src/test/java/io/codinghat/AppTest.java
+++ src/test/java/io/codinghat/AppTest.java
@@ -1,38 +1,16 @@
 package io.codinghat;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+	@Test
+    public void test1() {
+    	 System.out.println("test1");
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+	
+	@Test
+    public void test2() {
+    	 System.out.println("test2");
     }
 }
