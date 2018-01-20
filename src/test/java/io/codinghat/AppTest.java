d3c99ffd374360b4eb469e2c31547d4b684cb871
htech10 <himanshu.tech10@gmail.com>
2018-01-20 14:29:46 +1100

    Update AppTest.java

--- src/test/java/io/codinghat/AppTest.java
+++ src/test/java/io/codinghat/AppTest.java
@@ -6,11 +6,11 @@ public class AppTest
 {
 	@Test
     public void test1() {
-    	 System.out.println("test1");
+    	 System.out.println("test1 Poll SCM");
     }
 	
 	@Test
     public void test2() {
-    	 System.out.println("test2");
+    	 System.out.println("test2 poll scm");
     }
 }
